26f768b3d75be5d6cb33e439bb2fae7188e13921
ilhamaditya <adi3tz.ophelia@gmail.com>
2025-01-22 06:31:50 +0700

    tambah gitignore

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,3 @@
-// environment.js
 require('dotenv').config();  // Load .env file ke dalam environment variables
 
 const baseURL = process.env.BASE_URL || 'https://reqres.in/api'; // Default jika BASE_URL tidak ada
--- config/playwright.config.js
+++ config/playwright.config.js
@@ -1,4 +1,3 @@
-// playwright.config.js
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
